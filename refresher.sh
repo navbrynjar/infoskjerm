4f0fba26ebab6b78e22416eaff50e258c9c6efea
Brynjar Morka Mæhlum <brynjar.morka.mehlum@nav.no>
2024-05-03 11:01:06 +0200

    bugsmash

--- refresher.sh
+++ refresher.sh
@@ -1,7 +1,5 @@
-#!bin/bash
+#!/bin/bash
 echo "Refresherl for tabs"
 
 sleep 5
 lxterminal -t refresher -e 'sleep 2; cd /home/pi/Desktop/infoskjerm; git pull; sleep 2; . .venv/bin/activate; python3 infoskjerm_browser.py; exec bash'
-
-done
