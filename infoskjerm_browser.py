a3890880de51b688ea1810506bd4a8cc5595fe71
Brynjar Morka Mæhlum <brynjar.morka.mehlum@nav.no>
2024-10-04 12:41:46 +0200

    setter pausetid til 60s

--- infoskjerm_browser.py
+++ infoskjerm_browser.py
@@ -11,21 +11,21 @@ nettsider = {
     # "indikator1": "https://data.ansatt.nav.no/story/90383a5a-8cc9-42e2-a916-ec7dc90b3247/pages/detaljert/nav_historikk.html",
     # "ateam_quarto": "https://data.ansatt.nav.no/story/b54e4a30-7fca-491a-ab04-ac93795c7b37/pages/ovr_kompetanse.html",
     # "torden": "https://map.blitzortung.org/#7.01/59.024/11.082/0/1",
-    "damene": "https://data.ansatt.nav.no/story/7ea943c9-ae07-4d75-9b65-d775c05230dc/make_dashboard.html#kvinneandel-i-tech",
+    # "damene": "https://data.ansatt.nav.no/story/7ea943c9-ae07-4d75-9b65-d775c05230dc/make_dashboard.html#kvinneandel-i-tech",
     "yr_oslo": "https://www.yr.no/nb/v%C3%A6rvarsel/timetabell/1-72837/Noreg/Oslo/Oslo/Oslo?i=0",
     "nrk": "https://www.nrk.no/nyheter/",
     "quotes": "https://www.goodreads.com/quotes",
     "quotes": "https://zenquotes.io/",
     "delta": "https://delta.nav.no/",
-    "picsum1": "https://picsum.photos/1200/800",
-    "picsum2": "https://picsum.photos/1200/800",
-    "picsum3": "https://picsum.photos/1200/800",
+    "picsum1": "https://picsum.photos/1600/800",
+    "picsum2": "https://picsum.photos/1600/800",
+    "picsum3": "https://picsum.photos/1600/800",
 
 }
 
 mac = False
 cmd = "command" if mac else "ctrl"
-pause_tid = 10  # seconds
+pause_tid = 60  # seconds
 
 
 def main():
